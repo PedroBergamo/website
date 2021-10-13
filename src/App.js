3dccb616cac91f3daa9d6ebd13632d5060adb632
Pedro Bergamo <pedrobergamo89@gmail.com>
2021-10-13 16:12:26 +0300

    Addition of google analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import ReactGA from 'react-ga'
 import Navigation from "./components/Navigation.js";
 import Container from "./components/Container.js";
 import Footer from "./components/Footer.js";
+
+
+const TRACKING_ID = "G-W8BNQ6Q3SG";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (

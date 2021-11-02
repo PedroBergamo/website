bd727dd2456f81287d8ddcc6749a631757c20a2d
Pedro Bergamo <pedrobergamo89@gmail.com>
2021-11-02 12:12:42 +0200

    Setting up correct tracking ID

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactGA from 'react-ga'
 import Navigation from "./components/Navigation.js";
 import Container from "./components/Container.js";
 import Footer from "./components/Footer.js";
 
-
-const TRACKING_ID = "289719360";
-ReactGA.initialize(TRACKING_ID, {
-debug: true,
-titleCase: false,
-gaOptions: {
-userId: 210077702,
-},
-});
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 function App() {
+
+useEffect(() => {
+  ReactGA.initialize('UA-211790415-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+},[])
+
   return (
     <>
       <Navigation />

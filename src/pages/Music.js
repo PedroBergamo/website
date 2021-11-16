51b2a8faed291a0ebc55d17de93e1388016af1f0
Pedro Bergamo <pedrobergamo89@gmail.com>
2021-11-16 14:54:28 +0200

    update Santo

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import MusicPlayer from "../components/MusicPlayer.js";
 import VideoPlayer from "../components/VideoPlayer.js";
 

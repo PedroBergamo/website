2a924767083836ef9020129d661e55ff59c677d1
Pedro Bergamo <pedrobergamo89@gmail.com>
2021-11-26 07:08:57 +0200

    deployment music page

--- src/pages/MusicPage.js
+++ src/pages/MusicPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MusicList from "./content/MusicList.json"
 import {Redirect} from "react-router-dom"
 import "./pages.css"
-import { Container, Row, Col } from 'react-bootstrap';
 
 const MusicPage = (props) => {
   const ValidId = parseInt(props.match.params.id)

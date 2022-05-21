efd24c98a736f5dab18b4934e70d788585127460
Pedro Bergamo <pedrobergamo89@gmail.com>
2022-05-21 11:49:38 +0300

    Addition of photos

--- src/pages/Portifolio.js
+++ src/pages/Portifolio.js
@@ -1,19 +1,18 @@
 import "./pages.css"
 import {Col} from "react-bootstrap";
-import Photo from "../images/Photos/Pedro_candle.jpg"
+import Elias1 from "../images/clients/elias/Elias-1.jpg"
 import {Link} from "react-router-dom";
 
 const Portifolio = () => {
   return(
-    <div className="col_content">
+    <div className="client">
         <h1>Portifolio</h1>
         <p>Selection of clients I had the honor to work with</p>
         <h1>Elias Nieminen Ensemble</h1>
-        <img src={Photo} width="932" height="621" alt=""/>
+        <img src={Elias1} width="932" height="621" alt=""/>
         <p> lore ipsuloda aosd</p>
         <p>Link: behance/somewhere</p>
-
-        </div>
+    </div>
     )
 }
 
